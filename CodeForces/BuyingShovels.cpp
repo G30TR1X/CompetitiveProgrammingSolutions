725fa9e3944fcbbc742abcefc7ad7073bc60e22e
G30TR1X <gavriel10donovan@gmail.com>
2026-02-25 22:13:01 +0700

    Completed 1 problem

---
+++ CodeForces/BuyingShovels.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define ull unsigned long long
+#define vl vector<ll>
+#define vs vector<string>
+#define vc vector<char>
+#define vb vector<bool>
+#define vvl vector<vector<ll>>
+#define vvb vector<vector<bool>>
+
+const int MOD = 1e9 + 7;
+const int MAX_ARRAY_SIZE = 1e6 + 1;
+ll t,n,m,k,x,y,z,u,v;
+ll a[MAX_ARRAY_SIZE], b[MAX_ARRAY_SIZE];
+string s;
+
+void solve()
+{
+    cin >> n >> k;
+
+    if (k >= n)
+    {
+        cout << "1\n";
+        return;
+    }
+
+    if (n % k == 0)
+    {
+        cout << n / k << '\n';
+        return;
+    }
+
+    ll ans = n;
+    for (ll i = 2; i * i <= n && i <= k; ++i)
+    {
+        if (n % i == 0)
+        {
+            ans = min(ans, n / i);
+
+            if (n / i <= k)
+                ans = min(ans, i);
+        }
+    }
+
+    cout << ans << '\n';
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    //freopen("input.txt", "r", stdin);
+
+    cin >> t;
+    while (t--)
+        solve();
+
+    return 0;
+}
